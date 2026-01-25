5982d3f98907b8e0780209db1f94799cfa0dbd53
Mouhamadou Bamba DIOP [ext_diop004907 DRPS/PEX/SDN/SSD] <MouhamadouBamba.DIOP@orange-sonatel.com>
2026-01-25 21:09:08 +0000

    ajout de la fonctionnalite tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,7 +1,7 @@
 import json
 
 
-def ouvrir_inventaire(path="inventaires.json"):
+def ouvrir_inventaire(path="data/inventaires.json"):
     try:
         with open(path, "r", encoding="utf-8") as f:
             return json.load(f)
@@ -9,9 +9,24 @@ def ouvrir_inventaire(path="inventaires.json"):
         return {}
 
 
-def ecrire_inventaire(inventaire, path="inventaires.json"):
+def ecrire_inventaire(inventaire, path="data/inventaires.json"):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(inventaire, f, ensure_ascii=False, indent=4)
+
+
+def ouvrir_tresor(path="data/tresorie.txt"):
+    with open(path, "r", encoding="utf-8") as f:
+        tresorie = json.load(f)
+    return tresorie
+
+
+def ecrire_tresorerie(tresorie, path="data/tresorie.txt"):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(tresorie, f, ensure_ascii=False, indent=4)
+
+
+def afficher_tresorerie(tresorie):
+    print(f"Trésorerie actuelle : {tresorie:.2f} $")
 
 
 def afficher_inventaire(inventaire):
@@ -28,18 +43,23 @@ def recolter(inventaire, fruit, quantite):
     print(f"Récolté {quantite} {fruit} supplémentaires.")
 
 
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorie):
     if fruit in inventaire and inventaire[fruit] >= quantite:
         inventaire[fruit] -= quantite
+        tresorie += quantite * 1  # Supposons un prix de 1 $ par unité
         print(f"Vendu {quantite} {fruit}.")
+        return (inventaire, tresorie)
     else:
         print(f"Stock insuffisant pour {fruit}.")
 
 
 if __name__ == "__main__":
     inventaires = ouvrir_inventaire()
+    tresorerie = ouvrir_tresor()
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaires)
     recolter(inventaires, "mangues", 20)
-    vendre(inventaires, "bananes", 50)
+    inventaires, tresorerie = vendre(inventaires, "bananes", 5, tresorerie)
     afficher_inventaire(inventaires)
     ecrire_inventaire(inventaires)
+    ecrire_tresorerie(tresorerie)
